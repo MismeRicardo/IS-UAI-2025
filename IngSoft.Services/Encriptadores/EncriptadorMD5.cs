3ffc3f53e40cc948982009b5ef48f81c06efc9a9
MismeRicardo <misme.ricardo@gmail.com>
2025-08-18 20:54:28 -0300

    se agrego el encriptador MD5

---
+++ IngSoft.Services/Encriptadores/EncriptadorMD5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IngSoft.Services.Encriptadores
+{
+    public static class EncriptadorMD5
+    {
+        //TODO: Preguntar a los profes si deberiamos utilizar otro tipo de encriptacion por ejemplo SHA256 o Bcrypt.
+        public static string Encriptar(string textoPlano)
+        {
+            var md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(textoPlano));
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+        public static bool VerificarHash(string textoPlano, string hashGenerado)
+        {
+            string hashOfInput = Encriptar(textoPlano);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            
+            if (0 == comparer.Compare(hashOfInput, hashGenerado))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
