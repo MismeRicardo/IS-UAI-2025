6d1b21780aacff571afabb177bfa29a8b4460669
MismeRicardo <misme.ricardo@gmail.com>
2025-09-04 19:01:26 -0300

    add bitacora repository

---
+++ IngSoft.Repository/Factory/FactoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using IngSoft.DBConnection.Factory;
+
+namespace IngSoft.Repository.Factory
+{
+    public abstract class FactoryRepository
+    {
+        public static IBitacoraRepository CreateBitacoraRepository()
+        {
+            var connection = ConnectionFactory.CreateSqlServerConnection();
+            return new BitacoraRepository(connection);
+        }
+    }
+}
