b65478d767152b65a2d6dc08fb48b08eaff91437
MismeRicardo <misme.ricardo@gmail.com>
2025-09-04 19:01:08 -0300

    add bitacora services

---
+++ IngSoft.ApplicationServices/Implementation/BitacoraServices.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using IngSoft.Domain;
+using IngSoft.Repository;
+using IngSoft.Repository.Factory;
+
+namespace IngSoft.ApplicationServices
+{
+    public class BitacoraServices: IBitacoraServices
+    {
+        private readonly IBitacoraRepository _bitacoraRepository;
+        public BitacoraServices(IBitacoraRepository bitacoraRepository)
+        {
+            _bitacoraRepository = bitacoraRepository ?? FactoryRepository.CreateBitacoraRepository();
+        }
+        public void GuardarBitacora(Bitacora bitacora)
+        {
+            _bitacoraRepository.GuardarBitacora(bitacora);
+        }
+        public List<Bitacora> ObtenerBitacoras()
+        {
+            return _bitacoraRepository.ObtenerBitacoras();
+        }
+
+        public List<Bitacora> ObtenerBitacorasFiltradas(string filtro)
+        {
+            return _bitacoraRepository.ObtenerBitacorasFiltradas(filtro);
+        }
+    }
+}
