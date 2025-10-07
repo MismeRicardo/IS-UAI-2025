5c1a94c18f1ea3a5131db0cd48f0826d7c8beabe
MateoBaya <mateo.baya@gmail.com>
2025-10-06 21:20:01 -0300

    Agregados Nuevos Mensajes en Bitacora

--- IngSoft.ApplicationServices/Implementation/UsuarioServices.cs
+++ IngSoft.ApplicationServices/Implementation/UsuarioServices.cs
@@ -12,7 +12,7 @@ namespace IngSoft.ApplicationServices
     public class UsuarioServices: IUsuarioServices
     {
         private readonly IUsuarioRepository _usuarioRepository;
-        private Action<Usuario, string, string, TipoEvento> _registrarEnBitacora;
+        private static Action<Usuario, string, string, TipoEvento> _registrarEnBitacora;
 
         public UsuarioServices(IUsuarioRepository usuarioRepository)
         {
@@ -29,7 +29,7 @@ namespace IngSoft.ApplicationServices
             try
             {
                _usuarioRepository.GuardarUsuario(usuario);
-                _registrarEnBitacora(new Usuario { IdUsuario = SessionManager.GetUsuario().IdUsuario }, "Usuario creado/modificado exitosamente", "GuardarUsuario", TipoEvento.Message);
+               _registrarEnBitacora(new Usuario { IdUsuario = SessionManager.GetUsuario().IdUsuario }, "Usuario creado/modificado exitosamente", "GuardarUsuario", TipoEvento.Message);
             }
             catch(Exception)
             {
@@ -43,7 +43,7 @@ namespace IngSoft.ApplicationServices
             Usuario usuarioStored = ObtenerUsuario(usuario);
             if(usuarioStored!= null && usuarioStored.Bloqueado)
             {
-                _registrarEnBitacora(usuarioStored, "Intento de acceso con usuario bloqueado", "Login", TipoEvento.Error);
+                _registrarEnBitacora(usuarioStored, "Intento de acceso con usuario bloqueado", "Login", TipoEvento.Warning);
                 throw new UnauthorizedAccessException("El usuario se encuentra bloqueado.");
             }
             try
@@ -54,12 +54,13 @@ namespace IngSoft.ApplicationServices
             }
             catch(UnauthorizedAccessException)
             {
-                _registrarEnBitacora(usuarioStored, "Intento de acceso fallido", "Login", TipoEvento.Error);
+                _registrarEnBitacora(usuarioStored, "Intento de acceso fallido", "Login", TipoEvento.Warning);
                 _usuarioRepository.AumentarIntentosFallidos(usuario);
                 throw;
             }
-            catch(Exception)
+            catch(Exception e)
             {
+                _registrarEnBitacora(usuarioStored, "Error inesperado: "+e.Message, "Login", TipoEvento.Error);
                 throw;
             }
             return session;
@@ -71,17 +72,50 @@ namespace IngSoft.ApplicationServices
         }
         public Usuario ObtenerUsuario(Usuario usuario)
         {
-            return _usuarioRepository.ObtenerUsuario(usuario);
+            try
+            {
+                Usuario mUsuario = _usuarioRepository.ObtenerUsuario(usuario);
+                if (!(SessionManager.GetUsuario() is null))
+                {
+                    _registrarEnBitacora(SessionManager.GetUsuario() as Usuario, "Buscado Usuario "+usuario.Username, "ObtenerUsuario", TipoEvento.Message);
+                }
+                return mUsuario;
+            }
+            catch(Exception e)
+            {
+                _registrarEnBitacora(SessionManager.GetUsuario() as Usuario, "Error inesperado: " + e.Message, "Login", TipoEvento.Error);
+                throw;
+            }
         }
 
         public List<Usuario> ObtenerUsuarioFiltrados(string filtro)
         {
-            return _usuarioRepository.ObtenerUsuariosFiltrados(filtro);
+            try
+            {
+                List<Usuario> mUsuarios = _usuarioRepository.ObtenerUsuariosFiltrados(filtro);
+                _registrarEnBitacora(SessionManager.GetUsuario() as Usuario, "Buscados todos los Usuarios con filtro " +filtro , "ObtenerUsuariosFiltrados", TipoEvento.Message);
+                return mUsuarios;
+            }
+            catch (Exception e)
+            {
+                _registrarEnBitacora(SessionManager.GetUsuario() as Usuario, "Error inesperado: " + e.Message, "Login", TipoEvento.Error);
+                throw;
+            }
         }
 
         public List<Usuario> ObtenerUsuarios()
         {
-            return _usuarioRepository.ObtenerUsuarios();
+            try
+            {
+                List<Usuario> mUsuarios = _usuarioRepository.ObtenerUsuarios();
+                _registrarEnBitacora(SessionManager.GetUsuario() as Usuario, "Buscados todos los Usuarios", "ObtenerUsuariosFiltrados", TipoEvento.Message);
+                return mUsuarios;
+            }
+            catch (Exception e)
+            {
+                _registrarEnBitacora(SessionManager.GetUsuario() as Usuario, "Error inesperado: " + e.Message, "Login", TipoEvento.Error);
+                throw;
+            }
         }
 
         //private void LogOnBitacora(Usuario usuario, string descripcion, string origen,TipoEvento tipoEvento)
