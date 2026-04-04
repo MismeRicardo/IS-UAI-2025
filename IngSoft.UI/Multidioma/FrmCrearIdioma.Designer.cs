fa1c723523d94835496bf8424f7cd953768310c1
MateoBaya <78104751+MateoBaya@users.noreply.github.com>
2026-04-04 15:06:52 -0300

    Revert "Refactorizacion"

---
+++ IngSoft.UI/Multidioma/FrmCrearIdioma.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace IngSoft.UI.Multidioma
+{
+    partial class FrmCrearIdioma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCrearIdioma = new System.Windows.Forms.Label();
+            this.lblIdioma = new System.Windows.Forms.Label();
+            this.txtIdioma = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCrearIdioma
+            // 
+            this.lblCrearIdioma.AutoSize = true;
+            this.lblCrearIdioma.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCrearIdioma.Location = new System.Drawing.Point(313, 49);
+            this.lblCrearIdioma.Name = "lblCrearIdioma";
+            this.lblCrearIdioma.Size = new System.Drawing.Size(152, 29);
+            this.lblCrearIdioma.TabIndex = 0;
+            this.lblCrearIdioma.Text = "Crear Idioma";
+            // 
+            // lblIdioma
+            // 
+            this.lblIdioma.AutoSize = true;
+            this.lblIdioma.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdioma.Location = new System.Drawing.Point(236, 144);
+            this.lblIdioma.Name = "lblIdioma";
+            this.lblIdioma.Size = new System.Drawing.Size(58, 20);
+            this.lblIdioma.TabIndex = 1;
+            this.lblIdioma.Text = "Idioma";
+            // 
+            // txtIdioma
+            // 
+            this.txtIdioma.Location = new System.Drawing.Point(354, 144);
+            this.txtIdioma.Name = "txtIdioma";
+            this.txtIdioma.Size = new System.Drawing.Size(134, 22);
+            this.txtIdioma.TabIndex = 2;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(354, 200);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(134, 22);
+            this.txtCodigo.TabIndex = 4;
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(236, 200);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(61, 20);
+            this.lblCodigo.TabIndex = 3;
+            this.lblCodigo.Text = "Codigo";
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(318, 291);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(119, 31);
+            this.btnCrear.TabIndex = 5;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // FrmCrearIdioma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.txtIdioma);
+            this.Controls.Add(this.lblIdioma);
+            this.Controls.Add(this.lblCrearIdioma);
+            this.Name = "FrmCrearIdioma";
+            this.Text = "FrmCrearIdioma";
+            this.Load += new System.EventHandler(this.FrmCrearIdioma_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCrearIdioma;
+        private System.Windows.Forms.Label lblIdioma;
+        private System.Windows.Forms.TextBox txtIdioma;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Button btnCrear;
+    }
+}
